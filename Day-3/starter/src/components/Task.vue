f43031f5dc464da5b33a28153cb02e4cd51001b8
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2017-06-23 10:00:45 -0500

    Day-3 changes

---
+++ Day-3/starter/src/components/Task.vue
@@ -0,0 +1,77 @@
+<template>
+  <article class="media">
+    <div class="media-content">
+      <div class="content columns" >
+        <div class="column is-1 is-narrow">
+          
+          <input type="checkbox" v-model='isDone' @click='toggleIsDone'>
+        </div>
+        <h2 class='column is-three-quarters is-marginless' :class='isDone ? "is-done" : "is-not-done"'>{{ name }}</h2>
+        
+      </div>
+
+      <div class="column is-1 is-narrow">
+        <a class="button" @click="deleteTask" >Delete</a>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['done', 'name', 'index'],
+  name: 'task',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    isDone() {
+      return this.done
+    }
+  },
+  filters: {
+    capitalize(value) {
+      
+    }
+  },
+  methods: {
+    toggleIsDone() {
+      this.$emit('doneChanged', this.index);
+    },
+    deleteTask() {
+      this.$emit('taskDeleted', this.index);
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ Day-3/starter/src/components/TaskView.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="columns">
+    <div class="column is-half is-offset-one-quarter">
+      <section class="hero">
+        <div class="hero-body">
+          <h1 class="title has-text-centered">{{ title }}</h1>
+          <div class="field">
+            <p class="control">
+              <input class="input is-large" placeholder="Create a New task" type="text" v-on:keyup.enter='addTask' v-model='taskName'>
+            </p>
+          </div>
+        </div>
+      </section>
+      <div class="columns">
+        <div class=" block column is-half is-offset-one-quarter">
+          
+          <a class="button"  @click='showAllTasks'>Show All</a>
+          <a class="button"  @click='showDoneTasks'>Done</a>
+          <a class="button"  @click='showActiveTasks'>Active</a>
+          
+        </div>
+      </div>
+      <section class="hero">
+        <div class="hero-body columns is-multiline">
+          
+          <div class="box column is-10 is-offset-1"  v-for='(task, index) in tasksToShow'>
+             <task :done="task.done" :name="task.taskName" @doneChanged="handleDoneChange" :index="index" @taskDeleted="handleTaskDeleted"></task>
+          </div>
+          
+        </div>
+        
+      </section>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Task from './Task.vue'
+import Vue from 'vue'
+import axios from 'axios';
+
+export default {
+  name: 'task-view',
+  components: { Task },
+  data () {
+    return {
+        title:'TO DO',
+        taskName:'',
+        tasks: [],
+        showDone:true,
+        showAll:true
+    }
+  },
+  watch: {
+    showDone(newDone, oldDone) {
+      console.log(newDone);
+      console.log(oldDone);
+    }
+  },
+  mounted () {
+
+  },
+  created () {
+    axios.get('http://demo3594906.mockable.io/getTasks')
+    .then((response) => {
+      this.tasks = response.data.tasks;
+    })
+    .catch(error => console.log(error))
+  },
+  computed: {
+    tasksToShow() {
+      return this.tasks.filter((task) => task.done === this.showDone || this.showAll)
+    }
+  },
+  methods:{
+    addTask() {
+      this.tasks.push({
+        taskName:this.taskName,
+        done: false
+      })
+      this.taskName = ''
+    },
+    showAllTasks() {
+      this.showAll=true
+    },
+    showDoneTasks() {
+      this.showDone=true
+      this.showAll=false
+    },
+    showActiveTasks() {
+      this.showDone = false
+      this.showAll = false
+    },
+    handleDoneChange(index) {
+        const taskChanged = this.tasks[index];
+        taskChanged.done = !taskChanged.done
+        Vue.set(this.tasks, index, taskChanged);
+    },
+    handleTaskDeleted(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
